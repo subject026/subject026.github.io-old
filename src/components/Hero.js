90782601bf77125e47dc067c90622676b3e0649f
subject026 <sudobuildspeakers@gmail.com>
2019-12-03 12:27:40 +0000

    refactored animation to use timelines

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,16 @@
 import React, { useEffect, useRef } from "react"
 import styled from "styled-components"
-import { gsap, TimelineMax, Power3, Power1, Back, CSSPlugin } from "gsap"
 
 import LappyIcon from "./LappyIcon"
 import CvIcon from "./CvIcon"
 import GithubIcon from "./GithubIcon"
 import TwitterIcon from "./TwitterIcon"
-
-gsap.registerPlugin(CSSPlugin)
+import { animateIn } from "./animations"
 
 const Hero = styled.section`
   height: 100vh;
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
   overflow-x: hidden;
   overflow-y: hidden;
   position: relative;
@@ -25,7 +23,7 @@ const IconBar = styled.ul`
   right: 0;
   z-index: 10;
   list-style-type: none;
-  padding: 30px;
+  padding: 15px;
   li {
     display: inline-block;
     margin-left: 20px;
@@ -50,6 +48,10 @@ const IconBar = styled.ul`
 `
 
 const IconContainer = styled.div`
+  grid-column-start: 0;
+  grid-column-end: 1;
+  grid-row-start: 1;
+  grid-row-end: 2;
   opacity: 0;
   display: flex;
   align-items: center;
@@ -58,26 +60,30 @@ const IconContainer = styled.div`
     background-image: url("/images/Jt20.gif");
   }
 `
-
-const TextContainer = styled.div`
+const TextContainerTop = styled.div`
+  grid-column-start: 0;
+  grid-column-end: 1;
+  grid-row-start: 0;
+  grid-row-end: 1;
   background-color: #202020;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  transform: translateY(100%);
-  padding: 30px;
-  img {
-    width: 60px;
-    height: 60px;
-  }
+`
+
+const TextContainerBottom = styled.div`
+  grid-column-start: 0;
+  grid-column-end: 1;
+  grid-row-start: 2;
+  grid-row-end: 3;
+  background-color: #202020;
 `
 
 const TitleText = styled.h1`
-  width: 100%;
-  margin: 0;
   color: #c2c2c2;
-  font-size: 68px;
+  margin: 0;
+  padding: 15px;
+  font-size: 44px;
+  @media (min-width: 600px) {
+    font-size: 68px;
+  }
   font-weight: 700;
   line-height: 1.2;
 `
@@ -95,6 +101,7 @@ const SubtitleText = styled.p`
 
 export default () => {
   const iconContainerRef = useRef(null)
+  const heroRef = useRef(null)
   const textContainerRef = useRef(null)
   const iconRef = useRef(null)
   const titleTextRef = useRef(null)
@@ -102,96 +109,23 @@ export default () => {
   const githubIconRef = useRef(null)
   const cvIconRef = useRef(null)
   const twitterIconRef = useRef(null)
-  const tl = new TimelineMax()
-
-  const animateIn = () => {
-    // background containers
-    tl.to(iconContainerRef.current, 0.2, {
-      opacity: 1,
-    })
-    tl.to(
-      textContainerRef.current,
-      0.4,
-      {
-        y: 0,
-        ease: Power1.out,
-      },
-      "-=0.2"
-    )
-    // lappy
-    tl.from(
-      iconRef.current,
-      0.5,
-      {
-        y: "40%",
-        ease: Back.out,
-      },
-      "-=0.3"
-    )
-    tl.from(
-      iconRef.current,
-      0.5,
-      {
-        opacity: 0,
-        ease: Power3.easeOut,
-      },
-      "-=0.2"
-    )
-    // title text
-    tl.from(
-      titleTextRef.current,
-      0.4,
-      {
-        y: 50,
-        ease: Power3.easeOut,
-      },
-      "-=1"
-    )
-    tl.from(
-      titleTextRef.current,
-      0.4,
-      {
-        opacity: 0,
-        ease: Power3.easeOut,
-      },
-      "-=0.4"
-    )
-    // subtitle text
-    tl.from(
-      subtitleTextRef.current,
-      0.5,
-      {
-        x: 50,
-        opacity: 0,
-        ease: "power3.easeOut",
-      },
-      "-=0.25"
-    )
-
-    tl.staggerFrom(
-      [cvIconRef.current, githubIconRef.current, twitterIconRef.current],
-      0.6,
-      {
-        opacity: 0,
-        scale: 0,
-        x: 5,
-        y: 20,
-        ease: "elastic.out(1, 1)",
-        // delay: "-=1.6",
-      },
-      0.1,
-      "-=0.6"
-    )
-    // // icons
-    // animateIconBar(tl)
-  }
 
   useEffect(() => {
-    animateIn()
+    animateIn([
+      heroRef,
+      iconContainerRef,
+      textContainerRef,
+      iconRef,
+      titleTextRef,
+      subtitleTextRef,
+      githubIconRef,
+      cvIconRef,
+      twitterIconRef,
+    ])
   }, [])
 
   return (
-    <Hero>
+    <>
       <IconBar>
         <li>
           <a ref={cvIconRef} target="_blank" href="#">
@@ -209,19 +143,23 @@ export default () => {
           </a>
         </li>
       </IconBar>
-      <IconContainer ref={iconContainerRef}>
-        <LappyIcon iconRef={iconRef} />
-      </IconContainer>
-      <TextContainer ref={textContainerRef}>
-        <TitleText ref={titleTextRef}>
-          Hi,
-          <br />
-          I'm <Louis>Louis</Louis>.
-        </TitleText>
-        <SubtitleText ref={subtitleTextRef}>
-          I'm a web developer based in Cardiff, Wales.
-        </SubtitleText>
-      </TextContainer>
-    </Hero>
+      <Hero ref={heroRef}>
+        <IconContainer ref={iconContainerRef}>
+          <LappyIcon iconRef={iconRef} />
+        </IconContainer>
+        <TextContainerTop ref={textContainerRef}>
+          <TitleText ref={titleTextRef}>
+            Hi,
+            <br />
+            I'm <Louis>Louis</Louis>.
+          </TitleText>
+        </TextContainerTop>
+        <TextContainerBottom ref={textContainerRef}>
+          <SubtitleText ref={subtitleTextRef}>
+            I'm a web developer based in Cardiff, Wales.
+          </SubtitleText>
+        </TextContainerBottom>
+      </Hero>
+    </>
   )
 }
